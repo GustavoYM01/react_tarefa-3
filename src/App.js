c29296e90f8697f65d8badc7b3c7c6a17d76a64a
GustavoYM01 <gumatimoto@hotmail.com>
2022-07-13 14:52:25 -0300

    tarefa-3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const imgURL = "https://i.imgur.com/50NA7vr.gif";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Avançando em React</h1>
+      <img src={imgURL} alt="imagem animada" />
     </div>
   );
 }
